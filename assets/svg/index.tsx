10019d4efaf1b2568f8f3239f5480c628da9769b
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-04-26 19:34:48 +0330

    create svgfile for assets

---
+++ assets/svg/index.tsx
@@ -0,0 +1,8 @@
+export { default as ActiveHomeSvg } from "./ActiveHomeSvg";
+export { default as ActiveSearch } from "./ActiveSearch";
+export { default as ActiveStoreSvg } from "./ActiveStoreSvg";
+export { default as ActiveProfileSvg } from "./ActiveProfileSvg";
+export { default as HomeSvg } from "./HomeSvg";
+export { default as SearchSvg } from "./SearchSvg";
+export { default as StoreSvg } from "./StoreSvg";
+export { default as ProfileSvg } from "./ProfileSvg";
