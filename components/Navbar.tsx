5d1ee785961d91fcb635c8fc2d8771f04cd6432b
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-04-26 20:01:42 +0330

    add navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,25 @@
+import { ActiveStoreSvg, LogoSvg, MenuSvg } from "@/assets/svg";
+import React from "react";
+
+type NavbarProps = {};
+
+const Navbar: React.FC<NavbarProps> = () => {
+  return (
+    <div className="flex justify-between items-center w-full h-[72px] navbar_shadow">
+      <div className="navbar_bg_left flex items-center justify-center">
+        <div className="cursor-pointer">
+          <ActiveStoreSvg />
+        </div>
+      </div>
+      <div className="mt-8">
+        <LogoSvg />
+      </div>
+      <div className="navbar_bg_right flex items-center justify-center">
+        <div className="cursor-pointer">
+          <MenuSvg />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
