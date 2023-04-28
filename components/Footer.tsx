1f10c20cbb1613bd7915fdc4363f3f9664d1fc1d
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-04-28 17:20:05 +0330

    fix footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,6 @@
+import { useRouter } from "next/router";
+import React from "react";
+import ActiveLink from "./ui/ActiveLink";
 import {
   ActiveHomeSvg,
   HomeSvg,
@@ -8,10 +11,6 @@ import {
   ActiveProfileSvg,
   ProfileSvg,
 } from "@/assets/svg";
-import { useRouter } from "next/router";
-import React from "react";
-import ActiveLink from "./ui/ActiveLink";
-
 type FooterProps = {};
 
 const Footer: React.FC<FooterProps> = () => {
@@ -20,42 +19,7 @@ const Footer: React.FC<FooterProps> = () => {
   return (
     <div>
       <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white ">
-        <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
-          <ActiveLink
-            href="/"
-            className="inline-flex flex-col items-center justify-center px-5 py-2  border-gray-200 border-r  "
-            activeClassName="footer_bg border-none"
-          >
-            {router.pathname == "/" ? <ActiveHomeSvg /> : <HomeSvg />}
-            {router.pathname == "/" ? (
-              <span className="text-xs font-black py-2 text-white">خانه</span>
-            ) : null}
-          </ActiveLink>
-          <ActiveLink
-            href="/search"
-            className="inline-flex flex-col items-center justify-center px-5 py-2  border-gray-200 border-r  "
-            activeClassName="footer_bg border-none"
-          >
-            {router.pathname == "/search" ? <ActiveSearch /> : <SearchSvg />}
-            {router.pathname == "/search" ? (
-              <span className="text-xs font-black py-2 text-center text-white">
-                جست و جو
-              </span>
-            ) : null}
-          </ActiveLink>
-          {/* <hr className="border-x border-rose-500" /> */}
-          <ActiveLink
-            href="/store"
-            className="inline-flex flex-col items-center justify-center px-5 py-2 border-gray-200 border-x  "
-            activeClassName="footer_bg border-none"
-          >
-            {router.pathname == "/store" ? <ActiveStoreSvg /> : <StoreSvg />}
-            {router.pathname == "/store" ? (
-              <span className="text-xs font-black py-2 text-white">
-                فروشگاه
-              </span>
-            ) : null}
-          </ActiveLink>
+        <div className="grid h-full max-w-lg grid-cols-4 mx-auto pt-1 font-medium">
           <ActiveLink
             href="/profile"
             className="inline-flex flex-col items-center justify-center px-1 text-center border-gray-200"
@@ -70,6 +34,41 @@ const Footer: React.FC<FooterProps> = () => {
               <span className="text-xs py-2 font-black text-center text-white">
                 حساب کاربری
               </span>
+            ) : null}
+          </ActiveLink>
+
+          <ActiveLink
+            href="/store"
+            className="inline-flex flex-col items-center justify-center px-5 py-2 border-gray-200 border-x  "
+            activeClassName="footer_bg border-none"
+          >
+            {router.pathname == "/store" ? <ActiveStoreSvg /> : <StoreSvg />}
+            {router.pathname == "/store" ? (
+              <span className="text-xs font-black py-2 text-white">
+                فروشگاه
+              </span>
+            ) : null}
+          </ActiveLink>
+          <ActiveLink
+            href="/search"
+            className="inline-flex flex-col items-center justify-center px-5 py-2  border-gray-200 border-r  "
+            activeClassName="footer_bg border-none"
+          >
+            {router.pathname == "/search" ? <ActiveSearch /> : <SearchSvg />}
+            {router.pathname == "/search" ? (
+              <span className="text-xs font-black py-2 text-center text-white">
+                جست و جو
+              </span>
+            ) : null}
+          </ActiveLink>
+          <ActiveLink
+            href="/"
+            className="inline-flex flex-col items-center justify-center px-5 py-2  border-gray-200 border-r  "
+            activeClassName="footer_bg border-none"
+          >
+            {router.pathname == "/" ? <ActiveHomeSvg /> : <HomeSvg />}
+            {router.pathname == "/" ? (
+              <span className="text-xs font-black py-2 text-white">خانه</span>
             ) : null}
           </ActiveLink>
         </div>
