382ee7c84eca48e7cf53a5c85f4290740fc796bf
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-05-02 20:23:05 +0330

    complate search page

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,7 +1,33 @@
+import ContactUs from "@/components/ContactUs";
+import LstItmes from "@/components/LstItmes";
+import Navbar from "@/components/Navbar";
+import SearchBar from "@/components/SearchBar";
+import Card from "@/components/ui/Card";
+import Diveder from "@/components/ui/Diveder";
+import SeeProduct from "@/components/ui/SeeProduct";
 import React from "react";
 
 function Search() {
-  return <div>Search</div>;
+  return (
+    <div>
+      <Navbar />
+      <SearchBar />
+      <div className="flex  max-w-[390px] min-w-[390px] flex-wrap px-2 justify-center gap-2 mt-6">
+        <Card />
+        <Card />
+        <Card /> <Card />
+      </div>
+      <hr className="divedr opacity-25 my-10" />
+      <LstItmes />
+      <SeeProduct />
+      <hr className="divedr opacity-25 mb-10" />
+      <LstItmes />
+      <SeeProduct />
+      <hr className="divedr opacity-0 mb-4" />
+
+      <ContactUs />
+    </div>
+  );
 }
 
 export default Search;
