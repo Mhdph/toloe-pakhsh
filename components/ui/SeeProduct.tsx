8cdc4cf98f8ed5ba6ad1a0891ca5d7913b3c0125
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-05-02 20:22:18 +0330

    add Product button component

---
+++ components/ui/SeeProduct.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SeeProduct() {
+  return (
+    <div className="flex justify-center text-white">
+      <button className="btn_primary mt-48 mb-16 text-xs font-extrabold">
+        مشاهده محصولات
+      </button>
+    </div>
+  );
+}
+
+export default SeeProduct;
