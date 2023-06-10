7042c38edfbf14ecce23a8ff4c62b2cfbc376ee8
mhdph <mahdiparivash@protonmail.com>
2023-06-10 14:42:09 +0330

    upgrade to nextjs app router

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
-import { useRouter } from 'next/router';
-import React from 'react';
-import ActiveLink from './ui/ActiveLink';
+"use client";
+import React from "react";
+import ActiveLink from "./ui/ActiveLink";
 import {
   ActiveHomeSvg,
   HomeSvg,
@@ -10,52 +10,63 @@ import {
   StoreSvg,
   ActiveProfileSvg,
   ProfileSvg,
-} from '@/assets/svg';
+} from "@/assets/svg";
+import { usePathname } from "next/navigation";
 type FooterProps = {};
 
 const Footer: React.FC<FooterProps> = () => {
-  const router = useRouter();
+  const pathName = usePathname();
 
   return (
-    <div className='lg:hidden'>
-      <div className='fixed bottom-0 left-0 z-50 h-16 w-full bg-white '>
-        <div className='mx-auto grid h-full max-w-lg grid-cols-4 pt-1 font-medium'>
+    <div className="lg:hidden">
+      <div className="fixed bottom-0 left-0 z-50 h-16 w-full bg-white ">
+        <div className="mx-auto grid h-full max-w-lg grid-cols-4 pt-1 font-medium">
           <ActiveLink
-            href='/profile'
-            className='inline-flex flex-col items-center justify-center border-gray-200 px-1 text-center'
-            activeClassName='footer_bg border-none'
+            href="/profile"
+            className="inline-flex flex-col items-center justify-center border-gray-200 px-1 text-center"
+            activeClassName="footer_bg border-none"
           >
-            {router.pathname == '/profile' ? <ActiveProfileSvg /> : <ProfileSvg />}
-            {router.pathname == '/profile' ? (
-              <span className='py-2 text-center text-xs font-black text-white'>حساب کاربری</span>
+            {pathName == "/profile" ? <ActiveProfileSvg /> : <ProfileSvg />}
+            {pathName == "/profile" ? (
+              <span className="py-2 text-center text-xs font-black text-white">
+                حساب کاربری
+              </span>
             ) : null}
           </ActiveLink>
 
           <ActiveLink
-            href='/store'
-            className='inline-flex flex-col items-center justify-center border-x border-gray-200 px-5 py-2  '
-            activeClassName='footer_bg border-none'
+            href="/store"
+            className="inline-flex flex-col items-center justify-center border-x border-gray-200 px-5 py-2  "
+            activeClassName="footer_bg border-none"
           >
-            {router.pathname == '/store' ? <ActiveStoreSvg /> : <StoreSvg />}
-            {router.pathname == '/store' ? <span className='py-2 text-xs font-black text-white'>فروشگاه</span> : null}
-          </ActiveLink>
-          <ActiveLink
-            href='/search'
-            className='inline-flex flex-col items-center justify-center border-r border-gray-200  px-5 py-2  '
-            activeClassName='footer_bg border-none'
-          >
-            {router.pathname == '/search' ? <ActiveSearch /> : <SearchSvg />}
-            {router.pathname == '/search' ? (
-              <span className='py-2 text-center text-xs font-black text-white'>جست و جو</span>
+            {pathName == "/store" ? <ActiveStoreSvg /> : <StoreSvg />}
+            {pathName == "/store" ? (
+              <span className="py-2 text-xs font-black text-white">
+                فروشگاه
+              </span>
             ) : null}
           </ActiveLink>
           <ActiveLink
-            href='/'
-            className='inline-flex flex-col items-center justify-center border-r border-gray-200  px-5 py-2  '
-            activeClassName='footer_bg border-none'
+            href="/search"
+            className="inline-flex flex-col items-center justify-center border-r border-gray-200  px-5 py-2  "
+            activeClassName="footer_bg border-none"
           >
-            {router.pathname == '/' ? <ActiveHomeSvg /> : <HomeSvg />}
-            {router.pathname == '/' ? <span className='py-2 text-xs font-black text-white'>خانه</span> : null}
+            {pathName == "/search" ? <ActiveSearch /> : <SearchSvg />}
+            {pathName == "/search" ? (
+              <span className="py-2 text-center text-xs font-black text-white">
+                جست و جو
+              </span>
+            ) : null}
+          </ActiveLink>
+          <ActiveLink
+            href="/"
+            className="inline-flex flex-col items-center justify-center border-r border-gray-200  px-5 py-2  "
+            activeClassName="footer_bg border-none"
+          >
+            {pathName == "/" ? <ActiveHomeSvg /> : <HomeSvg />}
+            {pathName == "/" ? (
+              <span className="py-2 text-xs font-black text-white">خانه</span>
+            ) : null}
           </ActiveLink>
         </div>
       </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,6 @@
+"use client";
 import {
   AccoutIcon,
-  ActiveHomeSvg,
-  ActiveProfileSvg,
-  ActiveSearch,
   ActiveStoreSvg,
   BucketBlackIcon,
   BucketIcon,
@@ -14,94 +12,115 @@ import {
   ProfileSvg,
   SearchSvg,
   StoreSvg,
-} from '@/assets/svg';
-import SearchBarSvg from '@/assets/svg/SearchBarSvg';
-import { useRouter } from 'next/router';
-import React from 'react';
-import ActiveLink from './ui/ActiveLink';
+} from "@/assets/svg";
+import SearchBarSvg from "@/assets/svg/SearchBarSvg";
+import { usePathname } from "next/navigation";
+import React from "react";
+import ActiveLink from "./ui/ActiveLink";
+import Link from "next/link";
+import { cn } from "@/lib/cn";
 
 type NavbarProps = {};
 
 const Navbar: React.FC<NavbarProps> = () => {
-  const router = useRouter();
-
+  const pathName = usePathname();
   return (
     <div>
-      <div className='navbar_shadow fixed z-50 flex h-[72px] w-full items-center justify-between bg-white lg:hidden'>
-        <div className='navbar_bg_right flex items-center justify-center'>
-          <div className='cursor-pointer'>
+      <div className="navbar_shadow fixed z-50 flex h-[72px] w-full items-center justify-between bg-white lg:hidden">
+        <div className="navbar_bg_right flex items-center justify-center">
+          <div className="cursor-pointer">
             <MenuSvg />
           </div>
         </div>
-        <div className='mt-8'>
+        <div className="mt-8">
           <LogoSvg />
         </div>
-        <div className='navbar_bg_left flex items-center justify-center'>
-          <div className='cursor-pointer'>
+        <div className="navbar_bg_left flex items-center justify-center">
+          <div className="cursor-pointer">
             <ActiveStoreSvg />
           </div>
         </div>
       </div>
-      <div className='hidden lg:inline'>
-        <div className='flex items-center  justify-between px-12 py-6'>
+      <div className="hidden lg:inline">
+        <div className="flex items-center  justify-between px-12 py-6">
           <LogoIcon />
-          <div className='flex items-center justify-between gap-16 text-[14px]'>
+          <div className="flex items-center justify-between gap-16 text-[14px]">
             <p>وبلاگ</p>
             <p>درباره ما</p>
             <p>سیستم کسب درامد</p>
           </div>
-          <div className='flex items-center gap-4'>
-            <div className='relative  '>
+          <div className="flex items-center gap-4">
+            <div className="relative  ">
               <input
-                type='text'
-                id='voice-search'
-                className='w-[352px] rounded-[18px] bg-gray-200 py-1.5 pr-10 outline-none'
-                placeholder='جستجو'
+                type="text"
+                id="voice-search"
+                className="w-[352px] rounded-[18px] bg-gray-200 py-1.5 pr-10 outline-none"
+                placeholder="جستجو"
               />
-              <div className='pointer-events-none absolute inset-y-0  right-0 flex items-center pr-3'>
+              <div className="pointer-events-none absolute inset-y-0  right-0 flex items-center pr-3">
                 <SearchBarSvg />
               </div>
             </div>
-            <div className='contact_us flex h-10 w-10 items-center justify-center rounded-full bg-red-700'>
+            <div className="contact_us flex h-10 w-10 items-center justify-center rounded-full bg-red-700">
               <BucketIcon />
             </div>
           </div>
         </div>
-        <div className='flex h-10 w-full flex-row-reverse justify-between bg-[#E9EAEA] px-12'>
-          <ActiveLink
-            href='/profile'
-            className='flex items-center gap-3 px-24 text-[14px] font-extrabold text-black-items'
-            activeClassName='bg-white rounded-t-3xl  text-[#F6622C] border-t border-t-[#F6622C]'
+        <div className="flex h-10 w-full flex-row-reverse justify-between bg-[#E9EAEA] px-12">
+          <Link
+            href="/profile"
+            className={cn(
+              pathName === "/profile"
+                ? "bg-white rounded-t-3xl  text-[#F6622C] border-t border-t-[#F6622C]"
+                : "text-black-items",
+              "flex items-center gap-3 px-24 text-[14px] font-extrabold"
+            )}
           >
-            {router.pathname == '/profile' ? <ProfileSvg /> : <AccoutIcon />}
-            <span className='py-2 text-center text-xs font-black'>حساب کاربری</span>
-          </ActiveLink>
+            {pathName == "/profile" ? <ProfileSvg /> : <AccoutIcon />}
+            <span className="py-2 text-center text-xs font-black">
+              حساب کاربری
+            </span>
+          </Link>
 
-          <ActiveLink
-            href='/store'
-            className='flex items-center gap-3 px-24 text-[14px] font-extrabold text-black-items'
-            activeClassName='bg-white rounded-t-3xl text-[#F6622C] border-t border-t-[#F6622C]'
+          <Link
+            href="/store"
+            className={cn(
+              pathName === "/store"
+                ? "bg-white rounded-t-3xl  text-[#F6622C] border-t border-t-[#F6622C]"
+                : "text-black-items",
+              "flex items-center gap-3 px-24 text-[14px] font-extrabold"
+            )}
           >
-            {router.pathname == '/store' ? <StoreSvg /> : <BucketBlackIcon />}
-            <span className='py-2 text-xs font-black'>فروشگاه</span>
-          </ActiveLink>
-          <ActiveLink
-            href='/search'
-            className='flex items-center gap-3 px-24 text-[14px] font-extrabold text-black-items'
-            activeClassName='bg-white rounded-t-3xl text-[#F6622C] border-t border-t-[#F6622C]'
+            {pathName == "/store" ? <StoreSvg /> : <BucketBlackIcon />}
+            <span className="py-2 text-xs font-black">فروشگاه</span>
+          </Link>
+          <Link
+            href="/search"
+            className={cn(
+              pathName === "/search"
+                ? "bg-white rounded-t-3xl  text-[#F6622C] border-t border-t-[#F6622C]"
+                : "text-black-items",
+              "flex items-center gap-3 px-24 text-[14px] font-extrabold"
+            )}
           >
-            {router.pathname == '/search' ? <SearchSvg /> : <SearchBarSvg />}
-            <span className='py-2 text-center text-xs font-black'>جست و جو</span>
-          </ActiveLink>
-          <ActiveLink
-            href='/'
-            className='flex items-center gap-3 px-24 text-[14px] font-extrabold text-black-items'
-            activeClassName='bg-white rounded-t-3xl text-[#F6622C] border-t border-t-[#F6622C]'
+            {pathName == "/search" ? <SearchSvg /> : <SearchBarSvg />}
+            <span className="py-2 text-center text-xs font-black">
+              جست و جو
+            </span>
+          </Link>
+          <Link
+            href="/"
+            className={cn(
+              pathName === "/"
+                ? "bg-white rounded-t-3xl  text-[#F6622C] border-t border-t-[#F6622C]"
+                : "text-black-items",
+              "flex items-center gap-3 px-24 text-[14px] font-extrabold"
+            )}
           >
-            {router.pathname == '/' ? <HomeSvg /> : <HomeIcon />}
+            {pathName == "/" ? <HomeSvg /> : <HomeIcon />}
 
-            <span className='py-2 text-xs font-black'>خانه</span>
-          </ActiveLink>
+            <span className="py-2 text-xs font-black">خانه</span>
+          </Link>
         </div>
       </div>
     </div>
