832ffa31a59f1a67b0917dff4269dd1e7e13d93c
mahdi pariavsh <mahdiparivash@ptoonmail.com>
2023-06-10 14:42:09 +0330

    upgrade to nextjs app router

--- components/ui/ActiveLink.tsx
+++ components/ui/ActiveLink.tsx
@@ -1,6 +1,7 @@
-import { useRouter } from "next/router";
+import { cn } from "@/lib/cn";
 import Link, { LinkProps } from "next/link";
-import React, { PropsWithChildren, useState, useEffect } from "react";
+import { usePathname, useSelectedLayoutSegment } from "next/navigation";
+import { PropsWithChildren } from "react";
 
 type ActiveLinkProps = LinkProps & {
   className?: string;
@@ -9,42 +10,15 @@ type ActiveLinkProps = LinkProps & {
 
 const ActiveLink = ({
   children,
-  activeClassName,
+  href,
   className,
-  ...props
+  activeClassName,
 }: PropsWithChildren<ActiveLinkProps>) => {
-  const { asPath, isReady } = useRouter();
-  const [computedClassName, setComputedClassName] = useState(className);
-
-  useEffect(() => {
-    if (isReady) {
-      const linkPathname = new URL(
-        (props.as || props.href) as string,
-        location.href
-      ).pathname;
-      const activePathname = new URL(asPath, location.href).pathname;
-
-      const newClassName =
-        linkPathname === activePathname
-          ? `${className} ${activeClassName}`.trim()
-          : className;
-
-      if (newClassName !== computedClassName) {
-        setComputedClassName(newClassName);
-      }
-    }
-  }, [
-    asPath,
-    isReady,
-    props.as,
-    props.href,
-    activeClassName,
-    className,
-    computedClassName,
-  ]);
-
+  const pathname = usePathname();
+  const isActive = pathname;
+  console.log(pathname);
   return (
-    <Link className={computedClassName} {...props}>
+    <Link className={cn(isActive ? activeClassName : className)} href={href}>
       {children}
     </Link>
   );
--- components/ui/BackgroundTitle.tsx
+++ components/ui/BackgroundTitle.tsx
@@ -1,6 +1,7 @@
-import { ArrowBackSvg } from '@/assets/svg';
-import { useRouter } from 'next/router';
-import React from 'react';
+"use client";
+import { ArrowBackSvg } from "@/assets/svg";
+import { useRouter } from "next/navigation";
+import React from "react";
 
 type Props = {
   name: string;
@@ -12,13 +13,13 @@ function BackgroundTitle({ name }: Props) {
     router.back();
   }
   return (
-    <div className='pt-[74px] md:hidden'>
-      <div className='serach_bar flex h-[60px]  rounded-b-3xl'>
-        <div className='mr-4 flex items-center gap-2'>
+    <div className="pt-[74px] md:hidden">
+      <div className="serach_bar flex h-[60px]  rounded-b-3xl">
+        <div className="mr-4 flex items-center gap-2">
           <div onClick={handleBackClick}>
             <ArrowBackSvg />
           </div>
-          <p className='text-2xl font-normal text-white'> {name}</p>
+          <p className="text-2xl font-normal text-white"> {name}</p>
         </div>
       </div>
     </div>
