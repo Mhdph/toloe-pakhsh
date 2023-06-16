6a34065661bcc6d406ae9965b77c64eb6cb4c6d3
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-06-16 15:57:15 +0330

    complate zustand functionality

--- store/zustand.ts
+++ store/zustand.ts
@@ -1,9 +1,10 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
+
 interface Product {
   id: number;
   name: string;
-  quantity: number;
+  quantity: number; // Add quantity property to the Product type
 }
 
 interface ProductStore {
@@ -17,9 +18,26 @@ const useProductStore = create<ProductStore>()(
   devtools((set) => ({
     products: [],
     addProduct: (product) => {
-      set((state) => ({
-        products: [...state.products, product],
-      }));
+      set((state) => {
+        const existingProduct = state.products.find((p) => p.id === product.id);
+
+        if (existingProduct) {
+          // If product already exists, increase the quantity
+          const updatedProduct = {
+            ...existingProduct,
+            quantity: existingProduct.quantity + 1,
+          };
+
+          const updatedProducts = state.products.map((p) =>
+            p.id === product.id ? updatedProduct : p
+          );
+
+          return { products: updatedProducts };
+        }
+
+        // If product does not exist, add it with quantity = 1
+        return { products: [...state.products, { ...product, quantity: 1 }] };
+      });
     },
 
     removeProduct: (productId) => {
