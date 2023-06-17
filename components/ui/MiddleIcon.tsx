c465f9f6a27a4659715e42112d2ccfc934bf4f6e
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-06-17 18:32:58 +0330

    create middle icon component

---
+++ components/ui/MiddleIcon.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode } from "react";
+
+type MiddleIconProps = {
+  children: ReactNode;
+};
+
+const MiddleIcon: React.FC<MiddleIconProps> = ({ children }) => {
+  return (
+    <div className="flex h-9 w-9 items-center justify-center rounded-full border-[0.5px] border-black-items md:h-10 md:w-10">
+      <div className="">{children} </div>
+    </div>
+  );
+};
+export default MiddleIcon;
