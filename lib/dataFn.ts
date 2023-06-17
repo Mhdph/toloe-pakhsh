9431d4445fe8e19fb683e2720da3f3a3d23e55d5
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-06-17 18:33:32 +0330

    fix bugs

--- lib/dataFn.ts
+++ lib/dataFn.ts
@@ -9,6 +9,15 @@ export async function getAllProduct() {
   return res.json();
 }
 
+export async function getOneProduct(productId: number) {
+  const res = await fetch(`${baseUrl}/product/${productId}`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
 export async function getAllCategory() {
   const res = await fetch(`${baseUrl}/category`);
   if (!res.ok) {
