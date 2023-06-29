babedca278ccf2667776e0c9769d3ea42ffc1113
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-06-29 15:23:52 +0330

    fix button link in off hero

--- components/profile/ProfileInfo.tsx
+++ components/profile/ProfileInfo.tsx
@@ -1,16 +1,16 @@
-import { AvatarSvg } from '@/assets/svg';
-import React from 'react';
+import { AvatarIcon } from "@/assets/Icons";
+import React from "react";
 
 function ProfileInfo() {
   return (
-    <div className='pt-[72px] md:pt-8 '>
-      <div className='serach_bar flex h-full items-center gap-4 rounded-t-3xl px-5 py-4 md:flex-col'>
-        <AvatarSvg />
-        <div className='flex-1 text-xs font-normal text-white md:text-center'>
-          <p className='font-black'>نام و نام خانوادگی</p>
-          <hr className='borde my-1 border-solid border-white opacity-20' />
+    <div className="pt-[72px] md:pt-8 ">
+      <div className="serach_bar flex h-full items-center gap-4 rounded-t-3xl px-5 py-4 md:flex-col">
+        <AvatarIcon />
+        <div className="flex-1 text-xs font-normal text-white md:text-center">
+          <p className="font-black">نام و نام خانوادگی</p>
+          <hr className="borde my-1 border-solid border-white opacity-20" />
           <p>+۹۸ ۹۲۳ ۲۵۳ ۲۹ ۱۳</p>
-          <hr className='borde my-1 border-solid border-white opacity-20' />
+          <hr className="borde my-1 border-solid border-white opacity-20" />
           <p>آدرس: خراسان رضوی - مشهد - ...</p>
         </div>
       </div>
