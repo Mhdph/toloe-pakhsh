20eff4c334507b37bd96e76e6434a242d7f641b8
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-06-30 17:15:02 +0330

    fix show only 5 items in row

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import {
   MenuIcon,
   LogoIcon,
@@ -10,13 +10,14 @@ import {
   BucketBlackIcon,
   HomeIcon,
   HomeActiveIcon,
-} from "@/assets/Icons";
-import SearchBarSvg from "@/assets/svg/SearchBarSvg";
-import { usePathname } from "next/navigation";
-import React from "react";
-import Link from "next/link";
-import { cn } from "@/lib/cn";
-import { SearchIcon } from "lucide-react";
+} from '@/assets/Icons';
+import SearchBarSvg from '@/assets/svg/SearchBarSvg';
+import DesktopLogo from '@/assets/svg/LogoSvg';
+import {usePathname} from 'next/navigation';
+import React from 'react';
+import Link from 'next/link';
+import {cn} from '@/lib/cn';
+import {SearchIcon} from 'lucide-react';
 
 type NavbarProps = {};
 
@@ -24,100 +25,96 @@ const Navbar: React.FC<NavbarProps> = () => {
   const pathName = usePathname();
   return (
     <div>
-      <div className="navbar_shadow fixed z-50 flex pb-2 h-[72px] w-full items-center justify-between bg-white lg:hidden">
-        <div className="navbar_bg_right flex items-center justify-center">
-          <div className="cursor-pointer">
+      <div className='navbar_shadow fixed z-50 flex h-[72px] w-full items-center justify-between bg-white pb-2 lg:hidden'>
+        <div className='navbar_bg_right flex items-center justify-center'>
+          <div className='cursor-pointer'>
             <MenuIcon />
           </div>
         </div>
-        <Link href="/" className="mt-8">
+        <Link href='/' className='mt-8'>
           <LogoIcon />
         </Link>
-        <div className="navbar_bg_left flex items-center justify-center">
-          <div className="cursor-pointer">
+        <div className='navbar_bg_left flex items-center justify-center'>
+          <div className='cursor-pointer'>
             <StoreActiveIcon />
           </div>
         </div>
       </div>
-      <div className="hidden lg:inline">
-        <div className="flex items-center  justify-between px-12 py-6">
-          <LogoIcon />
-          <div className="flex items-center justify-between gap-16 text-[14px]">
+      <div className='hidden lg:inline'>
+        <div className='flex items-center  justify-between px-12 py-6'>
+          <DesktopLogo />
+          <div className='flex items-center justify-between gap-16 text-[14px]'>
             <p>وبلاگ</p>
             <p>درباره ما</p>
             <p>سیستم کسب درامد</p>
           </div>
-          <div className="flex items-center gap-4">
-            <div className="relative  ">
+          <div className='flex items-center gap-4'>
+            <div className='relative  '>
               <input
-                type="text"
-                id="voice-search"
-                className="w-[352px] rounded-[18px] bg-gray-200 py-1.5 pr-10 outline-none"
-                placeholder="جستجو"
+                type='text'
+                id='voice-search'
+                className='w-[352px] rounded-[18px] bg-gray-200 py-1.5 pr-10 outline-none'
+                placeholder='جستجو'
               />
-              <div className="pointer-events-none absolute inset-y-0  right-0 flex items-center pr-3">
+              <div className='pointer-events-none absolute inset-y-0  right-0 flex items-center pr-3'>
                 <SearchBarSvg />
               </div>
             </div>
-            <div className="contact_us flex h-10 w-10 items-center justify-center rounded-full bg-red-700">
+            <div className='contact_us flex h-10 w-10 items-center justify-center rounded-full bg-red-700'>
               <BucketIcon />
             </div>
           </div>
         </div>
-        <div className="flex h-10 w-full flex-row-reverse justify-between bg-[#E9EAEA] px-12">
+        <div className='flex h-10 w-full flex-row-reverse justify-between bg-[#E9EAEA] px-12'>
           <Link
-            href="/profile"
+            href='/profile'
             className={cn(
-              pathName === "/profile"
-                ? "bg-white rounded-t-3xl  text-[#F6622C] border-t border-t-[#F6622C]"
-                : "text-black-items",
-              "flex items-center gap-3 px-24 text-[14px] font-extrabold"
+              pathName === '/profile'
+                ? 'rounded-t-3xl border-t  border-t-[#F6622C] bg-white text-[#F6622C]'
+                : 'text-black-items',
+              'flex items-center gap-3 px-24 text-[14px] font-extrabold',
             )}
           >
-            {pathName == "/profile" ? <ProfileActiveIcon /> : <AccoutIcon />}
-            <span className="py-2 text-center text-xs font-black">
-              حساب کاربری
-            </span>
+            {pathName == '/profile' ? <ProfileActiveIcon /> : <AccoutIcon />}
+            <span className='py-2 text-center text-xs font-black'>حساب کاربری</span>
           </Link>
 
           <Link
-            href="/store"
+            href='/store'
             className={cn(
-              pathName === "/store"
-                ? "bg-white rounded-t-3xl  text-[#F6622C] border-t border-t-[#F6622C]"
-                : "text-black-items",
-              "flex items-center gap-3 px-24 text-[14px] font-extrabold"
+              pathName === '/store'
+                ? 'rounded-t-3xl border-t  border-t-[#F6622C] bg-white text-[#F6622C]'
+                : 'text-black-items',
+              'flex items-center gap-3 px-24 text-[14px] font-extrabold',
             )}
           >
-            {pathName == "/store" ? <StoreIcon /> : <BucketBlackIcon />}
-            <span className="py-2 text-xs font-black">فروشگاه</span>
+            {pathName == '/store' ? <StoreIcon /> : <BucketBlackIcon />}
+            <span className='py-2 text-xs font-black'>فروشگاه</span>
           </Link>
           <Link
-            href="/search"
+            href='/search'
             className={cn(
-              pathName === "/search"
-                ? "bg-white rounded-t-3xl  text-[#F6622C] border-t border-t-[#F6622C]"
-                : "text-black-items",
-              "flex items-center gap-3 px-24 text-[14px] font-extrabold"
+              pathName === '/search'
+                ? 'rounded-t-3xl border-t  border-t-[#F6622C] bg-white text-[#F6622C]'
+                : 'text-black-items',
+              'flex items-center gap-3 px-24 text-[14px] font-extrabold',
             )}
           >
-            {pathName == "/search" ? <SearchIcon /> : <SearchBarSvg />}
-            <span className="py-2 text-center text-xs font-black">
-              جست و جو
-            </span>
+            {pathName == '/search' ? <SearchIcon /> : <SearchBarSvg />}
+            <span className='py-2 text-center text-xs font-black'>جست و جو</span>
           </Link>
           <Link
-            href="/"
+            href='/'
             className={cn(
-              pathName === "/"
-                ? "bg-white rounded-t-3xl  text-[#F6622C] border-t border-t-[#F6622C]"
-                : "text-black-items",
-              "flex items-center gap-3 px-24 text-[14px] font-extrabold"
+              pathName === '/'
+                ? 'rounded-t-3xl border-t  border-t-[#F6622C] bg-white text-[#F6622C]'
+                : 'text-black-items',
+              'flex items-center gap-3 px-24 text-[14px] font-extrabold',
             )}
           >
-            {pathName == "/" ? <HomeActiveIcon /> : <HomeIcon />}
+            {pathName == '/' ? <HomeActiveIcon /> : <HomeIcon />}
 
-            <span className="py-2 text-xs font-black">خانه</span>
+            <span className='py-2 text-xs font-black'>خانه</span>
           </Link>
         </div>
       </div>
