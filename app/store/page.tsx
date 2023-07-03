e1ae174608374b17f927ab388cfb9f793a4be021
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-07-03 23:34:09 +0330

    use zustand for when user not login in the page

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -1,19 +1,20 @@
-import ContactUs from "@/components/ContactUs";
-import LatestBlog from "@/components/LatestBlog";
-import ListItems from "@/components/ListItems";
-import SearchBarWF from "@/components/ui/SearchBarWF";
-import { getAllProduct } from "@/lib/dataFn";
+import ContactUs from '@/components/ContactUs';
+import LatestBlog from '@/components/LatestBlog';
+import ListItems from '@/components/ListItems';
+import SpecialOffer from '@/components/SpecialOffer';
+import SearchBarWF from '@/components/ui/SearchBarWF';
+import {getAllProduct} from '@/lib/dataFn';
 
 export default async function Store() {
   const data = await getAllProduct();
   return (
     <div>
       <SearchBarWF />
-      <ListItems data={data.data} link="account" title="آخرین محصولات" />
-      {/* <SpecialOffer />
-      <ListItems link="account" title="شیرینی جات" />
-      <ListItems link="account" title="ترشی جات" /> */}
-      <LatestBlog />
+      <ListItems data={data.data} link='account' title='آخرین محصولات' />
+      <SpecialOffer data={data.data} />
+      {/* <ListItems link='account' title='شیرینی جات' />
+      <ListItems link='account' title='ترشی جات' /> */}
+      {/* <LatestBlog /> */}
       <ContactUs />
     </div>
   );
