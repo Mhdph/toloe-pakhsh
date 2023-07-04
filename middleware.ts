3cfa66d36c7d09a822b3a2ad245ec4f71922a70c
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-07-04 23:20:48 +0330

    update middleware with cookies

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,17 @@
 import {NextResponse} from 'next/server';
 import type {NextRequest} from 'next/server';
 
-// This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const token = 'hi';
-  const user = 'mamooli';
+  let token = request.cookies.get('token')?.value;
+  let userRole = request.cookies.get('userRole')?.value;
   if (!token) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
-  if (request.nextUrl.pathname.startsWith('/dashboard') && user !== 'mamooli') {
+  if (request.nextUrl.pathname.startsWith('/dashboard') && userRole !== 'admin') {
     return NextResponse.redirect(new URL('/login', request.url));
   }
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: ['/profile', '/dashboard/:path*'],
 };
