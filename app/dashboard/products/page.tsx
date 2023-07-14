cd49e616b4a8faf066d5dd021137986b8b676aff
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-07-14 23:38:09 +0330

    feat(dashboard):add product page

---
+++ app/dashboard/products/page.tsx
@@ -0,0 +1,24 @@
+import SearchBarSvg from '@/assets/svg/SearchBarSvg';
+import {Button} from '@/components/ui/Button';
+import {Input} from '@/components/ui/Input';
+import React from 'react';
+
+function page() {
+  return (
+    <div>
+      <div className='flex items-center gap-3'>
+        <div className='relative w-full '>
+          <Input className='rounded-[18px] bg-gray-200 py-1.5 pr-10' placeholder=' نام یا شماره همراه' />
+          <div className='pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3 outline-none'>
+            <SearchBarSvg />
+          </div>
+        </div>
+        <Button className='w-48'> جست و جو</Button>
+      </div>
+      <hr className='my-5 border border-main-red opacity-60' />
+      {/* <UserCard /> */}
+    </div>
+  );
+}
+
+export default page;
