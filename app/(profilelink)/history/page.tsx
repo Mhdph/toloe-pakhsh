d62f0307fb9959a5dafb30e60893621a94efeb71
mhdph <mahdiparivash@protonmail.com>
2023-07-18 11:14:57 +0330

    feat:change tab component in history page

--- app/(profilelink)/history/page.tsx
+++ app/(profilelink)/history/page.tsx
@@ -1,12 +1,32 @@
-import {Tabs} from '@/components/tabs';
+import {Tabs, TabsContent, TabsList, TabsTrigger} from '@/components/ui/Tabs';
 import BackgroundTitle from '@/components/ui/BackgroundTitle';
 import React from 'react';
+import HistoryCard from '@/components/ui/HistoryCard';
 
 function History() {
   return (
     <div>
       <BackgroundTitle name='تاریخچه سفارشات' />
-      <Tabs />
+      <Tabs defaultValue='sending' className='mt-4'>
+        <TabsList className=' flex justify-between gap-2 border-b-2 border-black-items border-opacity-50'>
+          <TabsTrigger value='returned'> مرجوع شده</TabsTrigger>
+          <TabsTrigger value='canceled'> لغو شده</TabsTrigger>
+          <TabsTrigger value='delivered'> تحویل شده</TabsTrigger>
+          <TabsTrigger value='sending'>در حال ارسال</TabsTrigger>
+        </TabsList>
+        <TabsContent value='sending'>
+          <HistoryCard color='primary' />
+        </TabsContent>
+        <TabsContent value='delivered'>
+          <HistoryCard color='delivered' />
+        </TabsContent>
+        <TabsContent value='canceled'>
+          <HistoryCard color='canceled' />
+        </TabsContent>
+        <TabsContent value='returned'>
+          <HistoryCard color='returned' />
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
