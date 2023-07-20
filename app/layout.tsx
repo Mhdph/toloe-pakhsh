9188163fcef368be372fcb310eb19949a9357a09
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-07-20 08:48:04 +0330

    feat: add toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,19 @@ import Navbar from '@/components/Navbar';
 import './globals.css';
 import Footer from '@/components/Footer';
 import QueryWrapper from '@/lib/QueryWrapper';
+import {Toaster} from 'react-hot-toast';
+import {Metadata} from 'next';
+
+export const metadata: Metadata = {
+  title: 'طلوع پخش',
+};
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <QueryWrapper>
       <html lang='en'>
         <body>
+          <Toaster />
           <Navbar />
           {children}
           <Footer />
