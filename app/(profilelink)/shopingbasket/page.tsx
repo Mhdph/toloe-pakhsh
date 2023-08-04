0cd574d828499d8e8e9465182faa56bcf6189ad1
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-08-04 14:30:08 +0330

    feat:show message when bucket empty

--- app/(profilelink)/shopingbasket/page.tsx
+++ app/(profilelink)/shopingbasket/page.tsx
@@ -1,32 +1,44 @@
+'use client';
+
 import ShoppingCard from '@/components/ShoppingCard';
 import Checkout from '@/components/shoppingBasket/Checkout';
 import OffCode from '@/components/shoppingBasket/OffCode';
 import BackgroundTitle from '@/components/ui/BackgroundTitle';
+import useProductStore from '@/store/zustand';
 
 function ShopingBasket() {
+  const products = useProductStore((state) => state.products);
+
   return (
     <div>
-      <BackgroundTitle name='سبد خرید' />
-      <div className='md:flex'>
-        <div className='mt-8 flex flex-col gap-2 px-4 md:flex-1'>
-          <ShoppingCard />
-        </div>
-        <div className='md:flex md:flex-col md:gap-4'>
-          <OffCode />
-          <Checkout />
-          <div className='background-gray mb-16 h-[222px] md:rounded-3xl'>
-            <div className='flex flex-col  gap-6 pt-1'>
-              <p className='mt-12 text-center text-4xl font-semibold'>تکرار خرید</p>
-              <p className='text-cente text-center text-xs font-normal'>
-                شما میتوانید سفارشات پیشین خود را تکرار کنید.
-              </p>
-              <div className='flex justify-center px-4  text-white md:justify-end'>
-                <button className='btn_primary h-9 w-full text-xs font-extrabold'>تاریخچه سفارشات</button>
+      {products.length > 0 ? (
+        <>
+          {' '}
+          <BackgroundTitle name='سبد خرید' />
+          <div className='md:flex'>
+            <div className='mt-8 flex flex-col gap-2 px-4 md:flex-1'>
+              <ShoppingCard />
+            </div>
+            <div className='md:flex md:flex-col md:gap-4'>
+              <OffCode />
+              <Checkout />
+              <div className='background-gray mb-16 h-[222px] md:rounded-3xl'>
+                <div className='flex flex-col  gap-6 pt-1'>
+                  <p className='mt-12 text-center text-4xl font-semibold'>تکرار خرید</p>
+                  <p className='text-cente text-center text-xs font-normal'>
+                    شما میتوانید سفارشات پیشین خود را تکرار کنید.
+                  </p>
+                  <div className='flex justify-center px-4  text-white md:justify-end'>
+                    <button className='btn_primary h-9 w-full text-xs font-extrabold'>تاریخچه سفارشات</button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
+        </>
+      ) : (
+        <p className='mt-8 flex items-center justify-center text-center text-4xl'>سبد خرید شما خالی است</p>
+      )}
     </div>
   );
 }
