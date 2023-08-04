2e356ab5b14de4326ceca972ab16e348f77976f6
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-08-04 21:12:24 +0330

    feat:update button component and add loading

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {Slot} from '@radix-ui/react-slot';
 import {cva, type VariantProps} from 'class-variance-authority';
-
+import {Loader2} from 'lucide-react';
 import {cn} from '@/lib/cn';
 
 const buttonVariants = cva(
@@ -31,18 +31,22 @@ const buttonVariants = cva(
   },
 );
 
-export interface ButtonProps
+export interface ButttonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean;
+  isLoading?: boolean;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({className, variant, size, asChild = false, ...props}, ref) => {
-    const Comp = asChild ? Slot : 'button';
-    return <Comp className={cn(buttonVariants({variant, size, className}))} ref={ref} {...props} />;
+const Button = React.forwardRef<HTMLButtonElement, ButttonProps>(
+  ({className, children, variant, isLoading, size, ...props}, ref) => {
+    return (
+      <button className={cn(buttonVariants({variant, size, className}))} ref={ref} disabled={isLoading} {...props}>
+        {isLoading ? <Loader2 className='mr-2 h-4 w-4 animate-spin' /> : null}
+        {children}
+      </button>
+    );
   },
 );
 Button.displayName = 'Button';
 
-export {Button, buttonVariants};
+export default Button;
