afb3915e4d686bb5d94954e115cd89002145e5fd
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-08-04 12:50:50 +0330

    fix: fix update category

--- app/dashboard/categories/page.tsx
+++ app/dashboard/categories/page.tsx
@@ -1,42 +1,59 @@
+'use client';
 import React from 'react';
-import sohanOrder from '@/assets/tempImages/sohanOrder.png';
 import Image from 'next/image';
 import {AddCategories} from '@/components/dashboard/AddCategories';
-import {Button} from '@/components/ui/Button';
 import {EditCategories} from '@/components/dashboard/EditCategories';
+import useGetCategoriesAndChilds from '@/service/category/useGetCategoriesandChilds';
+import {baseUrl} from '@/lib/config';
+import {Button} from '@/components/ui/Button';
+import {AddChildCategories} from '@/components/dashboard/AddChildCategories';
 
-function page() {
+function DashboardCategories() {
+  const {data} = useGetCategoriesAndChilds();
+  const [selectedCategory, setSelectedCategory] = React.useState<number | null>(null);
+  const [showModal, setShowModal] = React.useState<boolean>(false);
+
+  const openModal = (id: number) => {
+    setShowModal(true);
+    setSelectedCategory(id);
+  };
+
   return (
     <div>
       <div className='flex h-24 items-center justify-between rounded-t-3xl bg-[#253031] px-4 text-white'>
         <p>لیست دسته بندی ها</p>
         <AddCategories />
+        <AddChildCategories />
       </div>
-      <div className='bg-[#EAEBEB] p-4 text-black-items'>
-        <div className='grid grid-cols-3 rounded-3xl border border-black-items border-opacity-20 bg-white'>
-          <div className='col-span-1 flex flex-col items-center justify-center py-3'>
-            <Image src={sohanOrder} alt='category photo' height={300} width={300} />
-          </div>
-          <div className='col-span-2 flex flex-col px-4 py-4'>
-            <p className='mt-2 text-3xl'>سوهان</p>
-            <hr className='my-4 border-b border-b-black-items border-opacity-10' />
-            <p className='mb-4 text-xl font-semibold'>زیر مجموعه ها</p>
-            <div className='flex flex-wrap items-center gap-x-2 gap-y-6 text-sm font-extrabold'>
-              <div className='w-2/5 rounded-lg border-b'>برچسب</div>
-              <div className='w-2/5 rounded-lg border-b'>برچسب</div>
-              <div className='w-2/5 rounded-lg border-b'>برچسب</div>
-              <div className='w-2/5 rounded-lg border-b'>برچسب</div>
-              <div className='w-2/5 rounded-lg border-b'>برچسب</div>
-              <div className='w-2/5 rounded-lg border-b'>برچسب</div>
+      {data?.data.map((item) => (
+        <div key={item.id} className='bg-[#EAEBEB] p-4 text-black-items'>
+          <div className='grid grid-cols-3 rounded-3xl border border-black-items border-opacity-20 bg-white'>
+            <div className='col-span-1 flex flex-col items-center justify-center py-3'>
+              <Image src={baseUrl + item.picture} alt='category photo' height={300} width={300} />
             </div>
-            <div className='mt-6 flex justify-end'>
-              <EditCategories />
+            <div className='col-span-2 flex flex-col px-4 py-4'>
+              <p className='mt-2 text-3xl'>{item.name}</p>
+              <hr className='my-4 border-b border-b-black-items border-opacity-10' />
+              <p className='mb-4 text-xl font-semibold'>زیر مجموعه ها</p>
+              <div className='flex flex-wrap items-center gap-x-2 gap-y-6 text-sm font-extrabold'>
+                {item.chailds.map((item) => (
+                  <div key={item.id} className='w-2/5 rounded-lg border-b'>
+                    {item.name}
+                  </div>
+                ))}
+              </div>
+              <div className='mt-6 flex justify-end'>
+                <Button onClick={() => openModal(item.id)}>ویرایش</Button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ))}
+      {selectedCategory !== null && (
+        <EditCategories showModal={showModal} closeModal={setSelectedCategory} id={selectedCategory} />
+      )}
     </div>
   );
 }
 
-export default page;
+export default DashboardCategories;
--- service/category/useGetCategory.ts
+++ service/category/useGetCategory.ts
@@ -1,11 +1,11 @@
 import {useQuery} from '@tanstack/react-query';
 import APIClient from '../api-client';
-import User from '@/entities/user';
 import {CACHE_KEY_CATEGORY} from '../constants';
+import {Category} from '@/entities/category';
 
-const apiClient = new APIClient<User>('/category');
+const apiClient = new APIClient<Category>('/category');
 
-const useGetCategory = (slug: string) =>
+const useGetCategory = (slug: number) =>
   useQuery({
     queryKey: [CACHE_KEY_CATEGORY, slug],
     queryFn: () => apiClient.get(slug),
