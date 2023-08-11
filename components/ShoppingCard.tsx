055fe7ede3d7507783ec6fe23c87aaac9ffa2277
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-08-11 10:19:33 +0330

    feat:update shoping card with new api

--- components/ShoppingCard.tsx
+++ components/ShoppingCard.tsx
@@ -1,13 +1,15 @@
 'use client';
-import Image from 'next/image';
-import React from 'react';
 import {CloseIcon, MinusIcon, PlusIcon, StarIcon} from '@/assets/Icons';
-import MiddleIcon from './ui/MiddleIcon';
-import useProductStore from '@/store/zustand';
+import {UpdateCart} from '@/entities/Cart';
 import {baseUrl} from '@/lib/config';
 import useGetCart from '@/service/cart/useGetCart';
-import Cookies from 'js-cookie';
 import useUpdateCart from '@/service/cart/useUpdateCart';
+import useProductStore from '@/store/zustand';
+import {useQueryClient} from '@tanstack/react-query';
+import Cookies from 'js-cookie';
+import Image from 'next/image';
+import MiddleIcon from './ui/MiddleIcon';
+import {persianNumeralToNumber} from '@/helpers/PersianToEnglish';
 
 function ShoppingCard() {
   // user login or not
@@ -32,96 +34,112 @@ function ShoppingCard() {
     }
   };
 
-  const {mutate} = useUpdateCart();
+  const queryClient = useQueryClient();
+  const {mutate, isLoading} = useUpdateCart();
 
   // data backend
   const {data} = useGetCart();
-  const handleIncreaseData = (quantity: number, id: number) => {
-    const count = quantity + 1;
-    mutate({id, count}); // Call the updateProductQuantity function with the productId and updated quantity
+  const handleIncreaseData = (quantity: string, id: number) => {
+    const numericQuantity = persianNumeralToNumber(quantity);
+    const updatedCart: UpdateCart = {
+      count: +numericQuantity + 1, // The updated quantity
+      // Other properties of UpdateCart if needed
+    };
+
+    mutate({id, data: updatedCart});
   };
 
-  const handleDecreaseData = (quantity: number, id: number) => {
-    if (quantity > 1) {
-      const count = quantity - 1;
-      mutate({id, count}); // Call the updateProductQuantity function with the productId and updated quantity
-    } else if (quantity == 1) {
+  const handleDecreaseData = (quantity: string, id: number) => {
+    if (+quantity > 1) {
+      const updatedCart: UpdateCart = {
+        count: +quantity - 1, // The updated quantity
+        // Other properties of UpdateCart if needed
+      };
+      mutate({id, data: updatedCart}); // Call the updateProductQuantity function with the productId and updated quantity
+    } else if (+quantity === 1) {
       removeProduct(id);
     }
   };
+  console.log(data?.data);
+  if (isLoading) return <p>loading...</p>;
   return (
     <>
       {user !== undefined ? (
         <>
-          {data?.data[0].map((item) => (
-            <div
-              key={item.id}
-              className='flex h-[184px] w-full items-center rounded-3xl border border-black-items border-opacity-40 bg-white md:h-[148px]'
-            >
-              <div className='relative'>
-                <div onClick={() => removeProduct(item.id)} className='absolute right-4 top-4 cursor-pointer md:top-6'>
-                  <CloseIcon />
+          {data?.data.map((item) => {
+            return item.cartRows.map((item) => (
+              <div
+                key={item.cartId}
+                className='flex h-[184px] w-full items-center rounded-3xl border border-black-items border-opacity-40 bg-white md:h-[148px]'
+              >
+                <div className='relative'>
+                  <div
+                    onClick={() => removeProduct(item.cartId)}
+                    className='absolute right-4 top-4 cursor-pointer md:top-6'
+                  >
+                    <CloseIcon />
+                  </div>
+                  <Image
+                    src={baseUrl + item.productPicture}
+                    width={100}
+                    height={140}
+                    alt='product image'
+                    className='h-full md:pr-2'
+                  />
                 </div>
-                <Image
-                  src={baseUrl + item.productPicture}
-                  width={100}
-                  height={140}
-                  alt='product image'
-                  className='h-full md:pr-2'
-                />
+                <div className='h-full flex-1 px-2 pt-6 md:grid md:grid-cols-2 md:pt-0'>
+                  <div className='border-l-black-items border-opacity-10  md:flex md:flex-col md:gap-y-3 md:border-l md:py-4 md:pl-3'>
+                    <div className='flex items-center justify-between'>
+                      <p className='text-xs font-black md:text-sm'>{item.productName}</p>
+                      <StarIcon />
+                    </div>
+                    {/* <p className='mt-1 text-[10px] font-normal md:text-xs'>{item.}</p> */}
+                    <div className='hidden items-center justify-between md:mt-4 md:flex md:justify-center'>
+                      <div className='flex items-center justify-center gap-2'>
+                        <div onClick={() => handleDecreaseData(item.count, item.cartId)}>
+                          <MiddleIcon>
+                            <MinusIcon />
+                          </MiddleIcon>
+                        </div>
+                        <div className='numberItemBg flex h-9 w-[74px] items-center justify-center '>{item.count}</div>
+                        <div onClick={() => handleIncreaseData(item.count, item.cartId)}>
+                          <MiddleIcon>
+                            <PlusIcon />
+                          </MiddleIcon>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='md:flex md:flex-col md:gap-y-3 md:py-3 md:pr-3'>
+                    <hr className=' my-1 md:hidden' />
+                    <div className='flex items-center justify-between'>
+                      <p className='text-xs text-black-items md:text-sm'>واحد:</p>
+                      <div className='flex items-center'>
+                        <p className='text-xs font-black md:text-sm'>۱</p>
+                        <span className='mr-1 text-[10px] font-normal opacity-60 md:text-xs'>{item.productUnit}</span>
+                      </div>
+                    </div>
+                    <hr className=' my-1' />
+                    <div className='flex items-center justify-between'>
+                      <p className='text-xs text-black-items md:text-sm'>قیمت:</p>
+                      <div className='flex items-center'>
+                        <p className='text-xs font-black md:text-sm'>{item.price}</p>
+                        <span className='mr-1 text-[10px] font-normal opacity-60 md:text-xs'>تومان</span>
+                      </div>
+                    </div>
+                    <hr className=' my-1' />
+                    <div className='flex items-center justify-between text-[#F6622C]'>
+                      <p className='text-xs'>جمع خرید:</p>
+                      <div className='flex items-center'>
+                        <p className='text-base font-black'>{item.sumRow}</p>
+                        <span className='mr-1 text-[10px] font-normal text-black-items opacity-60'>تومان</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className='h-full flex-1 px-2 pt-6 md:grid md:grid-cols-2 md:pt-0'>
-                <div className='border-l-black-items border-opacity-10  md:flex md:flex-col md:gap-y-3 md:border-l md:py-4 md:pl-3'>
-                  <div className='flex items-center justify-between'>
-                    <p className='text-xs font-black md:text-sm'>{item.productName}</p>
-                    <StarIcon />
-                  </div>
-                  {/* <p className='mt-1 text-[10px] font-normal md:text-xs'>{item.}</p> */}
-                  <div className='hidden items-center justify-between md:mt-4 md:flex md:justify-center'>
-                    <div className='flex items-center justify-center gap-2'>
-                      <div onClick={() => handleDecrease(item.count, item.id)}>
-                        <MiddleIcon>
-                          <MinusIcon />
-                        </MiddleIcon>
-                      </div>
-                      <div className='numberItemBg flex h-9 w-[74px] items-center justify-center '>{item.count}</div>
-                      <div onClick={() => handleIncreaseData(item.count, item.id)}>
-                        <MiddleIcon>
-                          <PlusIcon />
-                        </MiddleIcon>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className='md:flex md:flex-col md:gap-y-3 md:py-3 md:pr-3'>
-                  <hr className=' my-1 md:hidden' />
-                  <div className='flex items-center justify-between'>
-                    <p className='text-xs text-black-items md:text-sm'>واحد:</p>
-                    <div className='flex items-center'>
-                      <p className='text-xs font-black md:text-sm'>۱</p>
-                      <span className='mr-1 text-[10px] font-normal opacity-60 md:text-xs'>{item.productUnit}</span>
-                    </div>
-                  </div>
-                  <hr className=' my-1' />
-                  <div className='flex items-center justify-between'>
-                    <p className='text-xs text-black-items md:text-sm'>قیمت:</p>
-                    <div className='flex items-center'>
-                      <p className='text-xs font-black md:text-sm'>{item.price}</p>
-                      <span className='mr-1 text-[10px] font-normal opacity-60 md:text-xs'>تومان</span>
-                    </div>
-                  </div>
-                  <hr className=' my-1' />
-                  <div className='flex items-center justify-between text-[#F6622C]'>
-                    <p className='text-xs'>جمع خرید:</p>
-                    <div className='flex items-center'>
-                      <p className='text-base font-black'>{item.sumRow}</p>
-                      <span className='mr-1 text-[10px] font-normal text-black-items opacity-60'>تومان</span>
-                    </div>
-                  </div>
-                </div>
-              </div>{' '}
-            </div>
-          ))}
+            ));
+          })}
         </>
       ) : (
         <>
