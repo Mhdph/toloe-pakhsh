d2a0cc0c29a50da1796880bad51752a63bee03ec
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-08-12 19:07:54 +0330

    fix:fix api url for delete category

--- service/category/useDeleteCategory.ts
+++ service/category/useDeleteCategory.ts
@@ -3,7 +3,7 @@ import APIClient from '../api-client';
 import {toast} from 'react-hot-toast';
 import {CACHE_KEY_CATEGORY} from '../constants';
 
-const apiClient = new APIClient<string | number>('/parent-category/delete');
+const apiClient = new APIClient<string | number>('/category/delete');
 
 const useDeleteCategory = () => {
   const queryClient = useQueryClient();
