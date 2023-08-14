582d9e5c8f7400618ecbd7ce6886b2260f221a1a
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-08-14 21:29:24 +0330

    feat:complete product details

--- app/store/[id]/page.tsx
+++ app/store/[id]/page.tsx
@@ -3,19 +3,19 @@ import React from 'react';
 import ContactUs from '@/components/ContactUs';
 import ListItems from '@/components/ListItems';
 import useSameProduct from '@/service/product/useSameProduct';
-import ProductDetails from '@/components/store/ProductTabs';
-import ProductTabs from '@/components/ListItems/store/ProductTabs';
+import ProductDetails from '@/components/store/ProductDetails';
+import ProductTabs from '@/components/store/ProductTabs';
 
 function SingleProduct() {
   const {data: product} = useSameProduct();
-  console.log(product);
+  console.log(product?.data);
   return (
     <div>
       <div>
-     <ProductDetails/> 
-     <ProductTabs/>
+        <ProductDetails />
+        <ProductTabs />
       </div>
-      <ListItems data={product?.data!} link='/' title='محصولات مشابه' />
+      <ListItems data={product?.data} link='/' title='محصولات مشابه' />
       <div className='hidden md:inline'>
         <ContactUs />
       </div>
--- components/store/ProductDetails.tsx
+++ components/store/ProductDetails.tsx
@@ -1,72 +1,106 @@
+/* eslint-disable @next/next/no-img-element */
 'use client';
 import StarSvg from '@/assets/Icons/StarIcon';
-import Image from 'next/image';
-import React from 'react';
+import React, {useState} from 'react';
 import MiddleIcon from '@/components/ui/MiddleIcon';
 import {FavouriteIcon, MiniBucketIcon, MinusIcon, PlusIcon, ShareIcon} from '@/assets/Icons';
 import useProduct from '@/service/product/useProduct';
+import {useParams} from 'next/navigation';
+import {baseUrl} from '@/lib/config';
+import useAddCart from '@/service/cart/useAddCart';
+import useProductStore from '@/store/zustand';
+import useAddFavouriteProduct from '@/service/product/useAddFavouriteProduct';
+import Cookies from 'js-cookie';
 
 function ProductDetails() {
-  const {id} = useParams()
-  const {data} = useProduct(id);
+  const [count, setCount] = useState(1);
+  const user = Cookies.get('token');
+  const {id} = useParams();
+  const {data} = useProduct(+id);
+  const addProduct = useProductStore((state) => state.addProduct);
+  const {mutate: addtoFavourite} = useAddFavouriteProduct();
+  const {mutate, isLoading} = useAddCart();
+  const handleAddToCart = () => {
+    const product = {
+      name: data?.name ?? 'Default Name',
+      id: data?.id ?? 0,
+      quantity: count,
+      unit: data?.unit ?? 'Default Unit',
+      unitCount: data?.unitCount ?? '0',
+      brand: data?.brand ?? 'Default Brand',
+      price: data?.price ?? 0,
+      picture: data?.picture ?? 'default-picture.jpg',
+    };
+    addProduct(product);
+  };
+  const addCardRow = () => {
+    mutate({
+      price: data?.price ?? 0,
+      productId: +id,
+      count,
+    });
+  };
+  const addFavourite = () => {
+    addtoFavourite({
+      favorite: [+id],
+    });
+  };
   return (
-      <div className='md:mt-7 md:grid md:grid-cols-3 md:px-10'>
-          <div className='md:col-span-1'>
-            <Image
-              src={baseUrl + data.data?.picture}
-              alt={data.data?.name!}
-              width={390}
-              height={200}
-              className='h-[200px]'
-            />
-          </div>
-          <div className='flex flex-col px-4 font-semibold md:col-span-2 md:rounded-3xl md:bg-white md:p-4 md:shadow-2xl'>
-            <div className='flex items-center justify-between'>
-              <p className=' text-4xl md:text-5xl '>{data.data?.name}</p>
-              <StarSvg />
-            </div>
-            <p className='mt-3 text-base md:text-xl'>{data.data?.description}</p>
-            <hr className='my-2' />
-            <div className='flex items-center justify-between'>
-              <p className='text-base'>برند:</p>
-              <p className='text-xs font-normal text-black-items'>{data.data?.brand}</p>
-            </div>
-            <hr className='my-2' />
-            <div className='flex items-center justify-between'>
-              <p className='text-base md:text-xl'>قیمت:</p>
-              <div className='flex items-center'>
-                <p className='text-xs font-black md:text-ca'>{data.data?.price}</p>
-                <span className='mr-1 text-[10px] font-normal opacity-60 md:text-xs'>تومان</span>
-              </div>
-            </div>
-            <hr className='my-2' />
-
-            <div className='flex items-center justify-between'>
-              <p className='text-base md:text-xl'>تعداد:</p>
-              <div className='flex items-center gap-2'>
-                <MiddleIcon>
-                  <MinusIcon />
-                </MiddleIcon>
-                <div className='numberItemBg flex h-9 w-[74px] items-center justify-center '>1</div>
-                <MiddleIcon>
-                  <PlusIcon />
-                </MiddleIcon>
-              </div>
-            </div>
-            <hr className='my-2' />
-            <div className='flex items-center justify-center gap-2 md:justify-start'>
-              <MiddleIcon>
-                <ShareIcon />
-              </MiddleIcon>
-              <MiddleIcon>
-                <FavouriteIcon />
-              </MiddleIcon>
-              <MiddleIcon>
-                <MiniBucketIcon />
-              </MiddleIcon>
-            </div>
+    <div className='md:mt-7 md:grid md:grid-cols-3 md:px-10'>
+      <div className='md:col-span-1'>
+        <img src={baseUrl + data?.picture} alt={data?.name!} width={390} height={200} className='h-[200px]' />
+      </div>
+      <div className='flex flex-col px-4 font-semibold md:col-span-2 md:rounded-3xl md:bg-white md:p-4 md:shadow-2xl'>
+        <div className='flex items-center justify-between'>
+          <p className=' text-4xl md:text-5xl '>{data?.name}</p>
+          <StarSvg />
+        </div>
+        <p className='mt-3 text-base md:text-xl'>{data?.description}</p>
+        <hr className='my-2' />
+        <div className='flex items-center justify-between'>
+          <p className='text-base'>برند:</p>
+          <p className='text-xs font-normal text-black-items'>{data?.brand}</p>
+        </div>
+        <hr className='my-2' />
+        <div className='flex items-center justify-between'>
+          <p className='text-base md:text-xl'>قیمت:</p>
+          <div className='flex items-center'>
+            <p className='text-xs font-black md:text-ca'>{data?.price}</p>
+            <span className='mr-1 text-[10px] font-normal opacity-60 md:text-xs'>تومان</span>
           </div>
         </div>
+        <hr className='my-2' />
+
+        <div className='flex items-center justify-between'>
+          <p className='text-base md:text-xl'>تعداد:</p>
+          <div className='flex items-center gap-2'>
+            <MiddleIcon>
+              <MinusIcon />
+            </MiddleIcon>
+            <div className='numberItemBg flex h-9 w-[74px] items-center justify-center '>{count}</div>
+            <MiddleIcon>
+              <PlusIcon />
+            </MiddleIcon>
+          </div>
+        </div>
+        <hr className='my-2' />
+        <div className='flex items-center justify-center gap-2 md:justify-start'>
+          <MiddleIcon>
+            <ShareIcon />
+          </MiddleIcon>
+          <MiddleIcon>
+            <div onClick={addFavourite}>
+              <FavouriteIcon />
+            </div>
+          </MiddleIcon>
+          <MiddleIcon>
+            <div onClick={user === undefined ? handleAddToCart : addCardRow}>
+              <MiniBucketIcon />
+            </div>
+          </MiddleIcon>
+        </div>
+      </div>
+    </div>
   );
 }
 
