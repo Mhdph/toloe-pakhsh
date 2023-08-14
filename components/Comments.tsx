00a4e20edc0d6cbfe68849db7525bf6cc918bc80
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-08-14 08:12:26 +0330

    feat:add star fot comment

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,17 +1,57 @@
+'use client';
 import React from 'react';
 import {Textarea} from './ui/TextArea';
 import Button from './ui/Button';
+import {useParams} from 'next/navigation';
+import {useMutation} from '@tanstack/react-query';
+import axios from 'axios';
+import {baseUrl} from '@/lib/config';
+import Rectangl from '@/assets/tempImages/Rectangl.png';
+import Image from 'next/image';
+import ReactStars from 'react-stars';
+import Cookies from 'js-cookie';
+
+interface AddComment {
+  star: number;
+  text: string;
+}
 
 function Comments() {
+  const {id} = useParams();
+  const [text, setText] = React.useState('');
+  const [star, setStar] = React.useState(5);
+  const token = Cookies.get('token');
+  const {mutate} = useMutation({
+    mutationFn: (data: AddComment) => {
+      return axios.post(`${baseUrl}/comment/add/${id}`, data, {
+        headers: {
+          authorization: 'Bearer ' + `${token}`,
+        },
+      });
+    },
+  });
+
+  const addComment = () => {
+    mutate({
+      text,
+      star,
+    });
+  };
+  const ratingChanged = (newRating: number) => {
+    setStar(newRating);
+  };
   return (
     <div className=''>
       <div className='pl-8 pr-2'>
-        <Textarea className='w-full' />
-        <Button className='mt-4 w-28 rounded-md'>ثبت</Button>
+        <Textarea onChange={(e) => setText(e.target.value)} className='w-full' />
+        <ReactStars count={5} onChange={ratingChanged} size={24} color2={'#F34834'} />
+        <Button onClick={addComment} className='mt-4 w-28 rounded-md'>
+          ثبت
+        </Button>
       </div>
       <div className='mt-6 flex flex-col gap-y-3 border-b border-black border-opacity-10 px-2'>
         <div className='flex flex-row-reverse items-center gap-2'>
-          <img src='' alt='' className='h-9 w-9 rounded-full' />
+          <Image src={Rectangl} alt='' className='h-9 w-9 rounded-full' />
           <p className='text-sm'>مهدی پریوش</p>
           <p className='text-xs text-slate-500'>دو هفته پیش</p>
         </div>
