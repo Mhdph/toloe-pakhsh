0fd74aa439be5a7f22a726b32d7466617dcacd12
mhdph <mahdiparivash@protonmail.com>
2023-08-19 14:06:37 +0330

    feat:fix last visited page

--- app/(profilelink)/lastvisit/page.tsx
+++ app/(profilelink)/lastvisit/page.tsx
@@ -6,34 +6,29 @@ import useLastVisitProduct from '@/service/product/useLastVisitProduct';
 
 function Latest() {
   const {data} = useLastVisitProduct();
-
+  console.log(data);
   return (
     <div>
       <BackgroundTitle name='بازدید های اخیر ' />
-      {data?.data.length !== undefined && data.data.length > 1 ? (
-        data?.data.map((item) => (
-          <>
-            <div className='mt-6 flex items-center justify-between px-4'>
-              <p className='text-xs font-normal tracking-wider text-black-items'>نمایش ۱ - ۱۶ از ۲۷۴ موارد</p>
 
-              <div className='flex h-[30px] w-[30px] items-center justify-center rounded-full border border-black-items'>
-                <div className=''>
-                  <UpDownIcon />
-                </div>
-              </div>
+      <>
+        <div className='mt-6 flex items-center justify-between px-4'>
+          <p className='text-xs font-normal tracking-wider text-black-items'>نمایش ۱ - ۱۶ از ۲۷۴ موارد</p>
+
+          <div className='flex h-[30px] w-[30px] items-center justify-center rounded-full border border-black-items'>
+            <div className=''>
+              <UpDownIcon />
             </div>
-            <div className='mt-6 grid grid-cols-2 gap-8 pr-2.5 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
-              {data?.data.map((item) => (
-                <div key={item.id}>
-                  <Card data={item} />
-                </div>
-              ))}{' '}
+          </div>
+        </div>
+        <div className='mt-6 grid grid-cols-2 gap-8 pr-2.5 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
+          {data?.data.map((item) => (
+            <div key={item.id}>
+              <Card data={item} />
             </div>
-          </>
-        ))
-      ) : (
-        <p className='mt-12 text-center md:text-4xl'>شما تاکنون محصولی را مشاهده نکرده اید </p>
-      )}
+          ))}{' '}
+        </div>
+      </>
     </div>
   );
 }
