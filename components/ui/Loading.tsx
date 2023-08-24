51b9dcfd1fc6e5dc84124a02e77bfc192db5564e
mhdph <mahdiparivash@protonmail.com>
2023-08-24 12:38:29 +0330

    fix:solve height issue of loading

--- components/ui/Loading.tsx
+++ components/ui/Loading.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Loading() {
   return (
-    <div className='flex items-center justify-center'>
+    <div className='flex h-screen items-center justify-center'>
       <div role='status'>
         <svg
           aria-hidden='true'
