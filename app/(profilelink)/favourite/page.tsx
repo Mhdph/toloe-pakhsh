92d6f435183e7fbb1dae008daf54cb24b6d3a4d3
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-08-25 18:53:10 +0330

    fix:fix reading twice data of favourite item

--- app/(profilelink)/favourite/page.tsx
+++ app/(profilelink)/favourite/page.tsx
@@ -10,26 +10,24 @@ function Favourite() {
     <div>
       <BackgroundTitle name='موارد دلخواه' />
       {data?.data.length !== undefined && data.data.length > 1 ? (
-        data?.data.map((item) => (
-          <>
-            <div className='mt-6 flex items-center justify-between px-4'>
-              <p className='text-xs font-normal tracking-wider text-black-items'>نمایش ۱ - ۱۶ از ۲۷۴ موارد</p>
+        <>
+          {/* <div className='mt-6 flex items-center justify-between px-4'>
+            <p className='text-xs font-normal tracking-wider text-black-items'>نمایش ۱ - ۱۶ از {data.count} موارد</p>
 
-              <div className='flex h-[30px] w-[30px] items-center justify-center rounded-full border border-black-items'>
-                <div className=''>
-                  <UpDownIcon />
-                </div>
+            <div className='flex h-[30px] w-[30px] items-center justify-center rounded-full border border-black-items'>
+              <div className=''>
+                <UpDownIcon />
               </div>
             </div>
-            <div className='mt-6 grid grid-cols-2 gap-8 pr-2.5 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
-              {data?.data.map((item) => (
-                <div key={item.id}>
-                  <Card data={item} />
-                </div>
-              ))}{' '}
-            </div>
-          </>
-        ))
+          </div> */}
+          <div className='mt-4 mt-6 grid grid-cols-2 gap-8 pr-2.5 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
+            {data?.data.map((item) => (
+              <div key={item.id}>
+                <Card data={item} />
+              </div>
+            ))}{' '}
+          </div>
+        </>
       ) : (
         <p className='mt-12 text-center md:text-4xl'>شما تاکنون محصولی را به عناوین مورد علاقه اضافه نکرده اید</p>
       )}
