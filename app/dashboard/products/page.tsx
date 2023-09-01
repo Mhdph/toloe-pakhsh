fb614c3219eeaab091a38d85681f5a00d9910571
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-09-01 12:56:51 +0330

    feat:add pagination to products page

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -4,12 +4,20 @@ import AddProduct from '@/components/dashboard/AddProduct';
 import DashboardProductCard from '@/components/dashboard/DashboardProductCard';
 import Button from '@/components/ui/Button';
 import {Input} from '@/components/ui/Input';
+import {PaginationList} from '@/components/ui/Pagination';
 import useProducts from '@/service/product/useProducts';
+import useProductQueryStore from '@/store/search';
 import React from 'react';
 
 function Products() {
   const {data} = useProducts();
+  const [page, setPage] = React.useState(1);
+  const {setSkip} = useProductQueryStore();
 
+  const onPageChange = (page: number) => {
+    setPage(page);
+    setSkip(page * 10);
+  };
   return (
     <div>
       <div className='flex items-center gap-3'>
@@ -23,13 +31,16 @@ function Products() {
       </div>
       <hr className='my-5 border border-main-red opacity-60' />
       <div className='grid grid-cols-4'>
-        <div className='  col-span-3'>
+        <div className=' col-span-3 flex flex-col items-center'>
           <div className='flex flex-wrap items-center gap-2'>
             {data?.data.map((item) => (
               <div key={item.id}>
                 <DashboardProductCard data={item} />
               </div>
             ))}
+          </div>
+          <div className='mt-2 flex flex-row-reverse justify-center'>
+            <PaginationList onPageChange={onPageChange} page={page} pageCount={data?.count} />
           </div>
         </div>
         <div className='col-span-1'>
