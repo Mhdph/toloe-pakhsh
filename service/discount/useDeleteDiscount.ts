812c132a824c278fabc6dfa2de5b3ca24ec476f5
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-09-03 00:50:36 +0330

    feat:update delte api service of discount

--- service/discount/useDeleteDiscount.ts
+++ service/discount/useDeleteDiscount.ts
@@ -4,13 +4,12 @@ import {Discount} from '@/entities/discount';
 import {CACHE_KEY_DISCOUNT} from '../constants';
 import {toast} from 'react-hot-toast';
 
-const apiClient = new APIClient<Discount>('/discount/delete');
+const apiClient = new APIClient<string | number>('/discount/delete');
 
-const useDeleteDiscount = (id: string) => {
+const useDeleteDiscount = () => {
   const queryClient = useQueryClient();
 
-  return useMutation<Discount, Error, Discount>({
-    mutationFn: () => apiClient.delete(id),
+  return useMutation<string | number, Error, string | number>((id) => apiClient.delete(id), {
     onSuccess: () => {
       queryClient.refetchQueries(CACHE_KEY_DISCOUNT);
       toast.success('تخفیف با موفقیت حذف شد');
