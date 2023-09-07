eecb4f13dda87265fb1a4783596fd560545f985d
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-09-07 19:52:32 +0330

    feat:add date picker in account

--- app/(profilelink)/account/page.tsx
+++ app/(profilelink)/account/page.tsx
@@ -5,24 +5,38 @@ import BackgroundTitle from '@/components/ui/BackgroundTitle';
 import Button from '@/components/ui/Button';
 import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from '@/components/ui/Form';
 import {Input} from '@/components/ui/Input';
-import User from '@/entities/user';
+import User, {UpdateUser} from '@/entities/user';
 import APIClient from '@/service/api-client';
 import {CACHE_KEY_USER} from '@/service/constants';
 import useUpdateUser from '@/service/user/useUpdateUser';
-import {userSchema} from '@/validation/user';
-import {zodResolver} from '@hookform/resolvers/zod';
+import {Label} from '@radix-ui/react-label';
 import {useQuery} from '@tanstack/react-query';
 import Cookies from 'js-cookie';
+import React from 'react';
 import {SubmitHandler, useForm} from 'react-hook-form';
-
+import DatePicker from 'react-multi-date-picker';
+import persian from 'react-date-object/calendars/persian';
+import persian_fa from 'react-date-object/locales/persian_fa';
+import {digitsFaToEn} from '@persian-tools/persian-tools';
 const apiClient = new APIClient<User>('/user/get');
 
 function Account() {
   const id = Cookies.get('userId')!;
+  let [date, setDate] = React.useState<any>();
   const form = useForm();
   const {mutate} = useUpdateUser();
   const updateUser: SubmitHandler<any> = async (data) => {
-    mutate(data);
+    mutate({
+      address: data.address,
+      city: data.city,
+      firstName: data.firstName,
+      lastName: data.lastName,
+      phone: data.phone,
+      birthDate: digitsFaToEn(date.toString()),
+      email: data.email,
+      nationalCode: data.nationalCode,
+      zipCode: data.zipCode,
+    });
   };
   const useUser = (slug: string) =>
     useQuery({
@@ -90,26 +104,16 @@ function Account() {
                 </FormItem>
               )}
             />
-            <FormField
-              control={form.control}
-              name='faBirthDate'
-              render={({field}) => (
-                <FormItem>
-                  <FormLabel>تاریخ تولد </FormLabel>
-                  <FormControl>
-                    <div className='md:w-[304px]'>
-                      <div className='relative'>
-                        <div className='absolute left-2 top-2 pr-3'>
-                          <EditIcon />
-                        </div>
-                        <Input placeholder={field.value} type='text' {...field} />
-                      </div>
-                    </div>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+            <div className='flex flex-col gap-1'>
+              <Label>تاریخ تولد</Label>
+              <DatePicker
+                className='font-Yekan'
+                inputClass='bg-background flex h-10 md:w-[304px] rounded-md border border-input px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50'
+                calendar={persian}
+                onChange={setDate}
+                locale={persian_fa}
+              />
+            </div>
           </div>
           <hr className='my-6 border opacity-50' />
           <div className='flex flex-col items-center gap-6 md:flex-row md:gap-12'>
