55856c818451c3e7848bfafc1b1b80d4b4b09c79
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-09-11 21:31:43 +0330

    feat:update category list

--- components/CategoryList.tsx
+++ components/CategoryList.tsx
@@ -13,16 +13,16 @@ const CategoryList: React.FC<CategoryListProps> = ({data}) => {
   return (
     <div className='mt-4 flex flex-col items-center justify-center'>
       <p className='mb-1 text-2xl font-normal text-black-items lg:mb-3'>دسته بندی محصولات</p>
-      <div className='flex flex-wrap items-center justify-center gap-2'>
+      <div className='flex  flex-wrap items-center justify-center gap-2'>
         {data?.map((item) => (
-          <div key={item.name} className='category_card'>
+          <div key={item.name} className='category_card h-[142px] w-[110px]  md:h-[244px] md:w-[196px] '>
             <Link href={`/product-category/${item.name}`}>
               <img
                 src={baseUrl + item.picture}
-                className='max-h-[110px] min-h-[110px] min-w-[110px] max-w-[110px] rounded-t-3xl '
+                className='md:min-[196px] max-h-[110px] min-h-[110px] min-w-[110px] max-w-[110px] rounded-t-3xl md:max-h-[196px] md:min-w-[196px] md:max-w-[196px] '
                 alt={item.name}
               />
-              <p className='mt-2 text-center text-xs font-extrabold text-black-items'>{item.name}</p>
+              <p className='mt-2 text-center text-xs font-extrabold text-black-items md:mt-3'>{item.name}</p>
             </Link>
           </div>
         ))}
