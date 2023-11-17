db39a27decaeb7ac3821148046c80715685fc354
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-11-17 17:32:00 +0330

    fix:fi redirect code

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -2,16 +2,17 @@
 import {useRouter} from 'next/navigation';
 import React from 'react';
 
-function DynamicPage() {
+function DynamicPage(context: any) {
   React.useEffect(() => {
     // Get the current URL path
     const currentPath = window.location.pathname;
-
+    const {res} = context;
     // Split the path and extract the value after "localhost:3000"
     const parts = currentPath.split('/');
     const valueAfterLocalhost = parts[1]; // Index 0 will be an empty string
-    window.location.replace(`https://mag.toloupakhsh.ir/${decodeURIComponent(valueAfterLocalhost)}`);
+    res.writeHead(301, {location: ` https://mag.toloupakhsh.ir/${decodeURIComponent(valueAfterLocalhost)} `});
   }, []);
+
   return <div></div>;
 }
 
