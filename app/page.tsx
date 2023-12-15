ff83fa5099eaf85fa00a9b7ad24c6f62d281db63
JamalSanei <jamal.sanei@gmail.com>
2023-12-15 22:51:35 +0330

    feat:add offerHero-2 & zero postCost for product.id =1

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import SpecialOffer from '@/components/SpecialOffer';
 import SliderImage from '@/components/ui/Slider';
 import {getAllCategory} from '@/service/category';
 import {getAllOffProduct, getAllProduct} from '@/service/product';
+import OfferHero2 from '@/components/OfferHero-2';
 
 export default async function Home() {
   const data = await getAllProduct();
@@ -17,6 +18,7 @@ export default async function Home() {
   return (
     <main className='flex-col pt-1 md:pt-0'>
       <SliderImage />
+      <OfferHero2 />
       <CategoryList data={categoryData.data} />
       <OfferHero />
       <ListItems data={data.data} link='' title='آخرین محصولات' />
