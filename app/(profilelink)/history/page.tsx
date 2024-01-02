2d365f0e7db631730c399844676c430058f48a60
JamalSanei <jamal.sanei@gmail.com>
2024-01-03 02:00:03 +0330

    add: posted state cart in user panel and add field tracking code

--- app/(profilelink)/history/page.tsx
+++ app/(profilelink)/history/page.tsx
@@ -13,10 +13,14 @@ function History() {
           <TabsTrigger value='returned'> مرجوع شده</TabsTrigger>
           <TabsTrigger value='canceled'> لغو شده</TabsTrigger>
           <TabsTrigger value='delivered'> تحویل شده</TabsTrigger>
+          <TabsTrigger value='posted'>ارسال شده</TabsTrigger>
           <TabsTrigger value='sending'>در حال ارسال</TabsTrigger>
         </TabsList>
         <TabsContent value='sending'>
           <HistoryCard color='primary' state='sending' label='در حال ارسال' />
+        </TabsContent>
+        <TabsContent value='posted'>
+          <HistoryCard color='primary' state='posted' label='ارسال شده' />
         </TabsContent>
         <TabsContent value='delivered'>
           <HistoryCard color='delivered' state='delivered' label='تحویل شده' />
